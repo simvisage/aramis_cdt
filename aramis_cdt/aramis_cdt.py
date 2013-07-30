f765254f0d9ca75f061c5afc9012c01939f9121b
kelidas <kelidas@centrum.cz>
2013-07-30 07:56:25 +0200

    new variables and methods for crack width and crack initiation added

--- aramis_cdt/aramis_cdt.py
+++ aramis_cdt/aramis_cdt.py
@@ -55,6 +55,7 @@ class AramisStep(HasTraits):
 class AramisCDT(HasTraits):
     '''Crack Detection Tool for detection of cracks, etc. from Aramis data.
     '''
+
     aramis_info = Instance(AramisInfo)
 
     step_list = DelegatesTo('aramis_info')
@@ -66,19 +67,19 @@ class AramisCDT(HasTraits):
     '''Time step used for the evaluation.
     '''
 
-    n_px_facette_size_x = Int(19, params_changed=True)
+    n_px_facet_size_x = Int(19, params_changed=True)
     '''Size of a facet in x-direction defined in numbers of pixels
     '''
 
-    n_px_facette_distance_x = Int(15, params_changed=True)
+    n_px_facet_distance_x = Int(15, params_changed=True)
     '''Distance between the mid-points of two facets in x-direction
     '''
 
-    n_px_facette_size_y = Int(19, params_changed=True)
+    n_px_facet_size_y = Int(19, params_changed=True)
     '''Size of a facet in y-direction defined in numbers of pixels
     '''
 
-    n_px_facette_distance_y = Int(15, params_changed=True)
+    n_px_facet_distance_y = Int(15, params_changed=True)
     '''Distance between the mid-points of two facets in y-direction
     '''
 
@@ -94,12 +95,13 @@ class AramisCDT(HasTraits):
 #        return ceil( float( self.n_px_f / self.n_px_a )
     integ_radius = Int(11, params_changed=True)
 
+    transform_data = Bool(False, params_changed=True)
+    '''Switch data transformation before analysis
+    '''
+
     #===========================================================================
     # Undeformed state parameters
     #===========================================================================
-    # TODO: length of header change depending on number of AD-channels (or other additional informations?)
-    # if the header line starts with # than this is not necessary
-    header_length = Int
 
     input_array_init = Property(Array, depends_on='aramis_info.data_dir')
     '''Array of values for undeformed state in the first step.
@@ -194,7 +196,7 @@ class AramisCDT(HasTraits):
     @cached_property
     def _get_x_idx_init(self):
         return (np.arange(self.n_x_init)[np.newaxis, :] *
-                np.ones(self.n_y_init)[:, np.newaxis])
+                np.ones(self.n_y_init)[:, np.newaxis]).astype(int)
 
     y_idx_init = Property(Int, depends_on='aramis_info.data_dir')
     '''Indices in the first column of the undeformed state starting with zero.
@@ -209,28 +211,28 @@ class AramisCDT(HasTraits):
     '''
     @cached_property
     def _get_x_arr_init(self):
-        return self.data_array_init[2, :, :]
+        return self.data_array_init[0, :, :]
 
     y_arr_init = Property(Array, depends_on='aramis_info.data_dir')
     '''Array of y-coordinates in undeformed state
     '''
     @cached_property
     def _get_y_arr_init(self):
-        return self.data_array_init[3, :, :]
+        return self.data_array_init[1, :, :]
 
     z_arr_init = Property(Array, depends_on='aramis_info.data_dir')
     '''Array of z-coordinates in undeformed state
     '''
     @cached_property
     def _get_z_arr_init(self):
-        return self.data_array_init[4, :, :]
+        return self.data_array_init[2, :, :]
 
     length_x_init = Property(Array, depends_on='aramis_info.data_dir')
     '''Length of the specimen in x-direction
     '''
     @cached_property
     def _get_length_x_init(self):
-        return self.x_arr_init.max() - self.x_arr_init.min()
+        return np.nanmax(self.x_arr_init) - np.nanmin(self.x_arr_init)
 
     length_y_init = Property(Array, depends_on='aramis_info.data_dir')
     '''Length of the specimen in y-direction
@@ -256,8 +258,6 @@ class AramisCDT(HasTraits):
     @cached_property
     def _get_input_array(self):
         return self._load_step_data(self.evaluated_step)
-
-    transform_data = Bool(False, params_changed=True)
 
     data_array = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
     '''Array of Aramis exported data 
@@ -286,7 +286,6 @@ class AramisCDT(HasTraits):
             rot_y = np.array([[np.cos(alpha), 0, -np.sin(alpha)],
                               [0, 1, 0],
                               [np.sin(alpha), 0, np.cos(alpha)]])
-            shape = data_arr.shape
 
             # rotate counterclockwise about the axis x
             linreg = stats.linregress(self.data_array_init[1, :, :][mask].ravel(),
@@ -434,7 +433,9 @@ class AramisCDT(HasTraits):
     def _get_uz_arr(self):
         return self.data_array[5, :, :]
 
-    d_ux_tol = Float(0.00)
+    d_ux_threshold = Float(0.0)
+    '''The first derivative of displacement in x-direction threshold
+    '''
 
     d_ux_arr = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
     '''The first derivative of displacement in x-direction
@@ -445,7 +446,7 @@ class AramisCDT(HasTraits):
         # cutoff the negative strains - noise
         d_arr[ d_arr < 0.0 ] = 0.0
         # eliminate displacement jumps smaller then specified tolerance
-        d_arr[ d_arr < self.d_ux_tol ] = 0.0
+        d_arr[ d_arr < self.d_ux_threshold ] = 0.0
         return d_arr
 
     dd_ux_arr = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
@@ -486,7 +487,7 @@ class AramisCDT(HasTraits):
         # cutoff the negative strains - noise
         d_arr[ d_arr < 0.0 ] = 0.0
         # eliminate displacement jumps smaller then specified tolerance
-        d_arr[ d_arr < self.d_ux_tol ] = 0.0
+        d_arr[ d_arr < self.d_ux_threshold ] = 0.0
         return d_arr
 
     d_uz_arr = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
@@ -498,29 +499,57 @@ class AramisCDT(HasTraits):
         # cutoff the negative strains - noise
         d_arr[ d_arr < 0.0 ] = 0.0
         # eliminate displacement jumps smaller then specified tolerance
-        d_arr[ d_arr < self.d_ux_tol ] = 0.0
+        d_arr[ d_arr < self.d_ux_threshold ] = 0.0
         return d_arr
 
     #===========================================================================
     # Crack detection
     #===========================================================================
+    dd_ux_threshold = Float(0.0)
+    '''The second derivative of displacement in x-direction threshold
+    '''
+
+    ddd_ux_threshold = Float(-0.01)
+    '''The third derivative of displacement in x-direction threshold
+    '''
+
+    dd_ux_avg_threshold = Float(0.0)
+    '''Average of he second derivative of displacement in x-direction threshold
+    '''
+
+    ddd_ux_avg_threshold = Float(-0.01)
+    '''Average the third derivative of displacement in x-direction threshold
+    '''
+
     crack_filter = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
     @cached_property
     def _get_crack_filter(self):
         dd_ux_arr = self.dd_ux_arr
         ddd_ux_arr = self.ddd_ux_arr
-        return ((dd_ux_arr[:, 1:] * dd_ux_arr[:, :-1] < 0.0) *
-                ((ddd_ux_arr[:, 1:] + ddd_ux_arr[:, :-1]) / 2.0 < -0.005))
+        return ((dd_ux_arr[:, 1:] * dd_ux_arr[:, :-1] < self.dd_ux_threshold) *
+                ((ddd_ux_arr[:, 1:] + ddd_ux_arr[:, :-1]) / 2.0 < self.ddd_ux_threshold))
+
+    number_of_subcracks = Property(Int, depends_on='aramis_info.data_dir, +params_changed')
+    '''Number of sub-cracks
+    '''
+    def _get_number_of_subcracks(self):
+        return np.sum(self.crack_filter)
 
     crack_filter_avg = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
     @cached_property
     def _get_crack_filter_avg(self):
         dd_ux_arr_avg = self.dd_ux_arr_avg
         ddd_ux_arr_avg = self.ddd_ux_arr_avg
-        crack_filter_avg = ((dd_ux_arr_avg[1:] * dd_ux_arr_avg[:-1] < 0.0) *
-                           ((ddd_ux_arr_avg[1:] + ddd_ux_arr_avg[:-1]) / 2.0 < -0.01))
+        crack_filter_avg = ((dd_ux_arr_avg[1:] * dd_ux_arr_avg[:-1] < self.dd_ux_avg_threshold) *
+                           ((ddd_ux_arr_avg[1:] + ddd_ux_arr_avg[:-1]) / 2.0 < self.ddd_ux_avg_threshold))
         print "number of cracks determined by 'crack_filter_avg': ", np.sum(crack_filter_avg)
         return crack_filter_avg
+
+    number_of_cracks_avg = Property(Int, depends_on='aramis_info.data_dir, +params_changed')
+    '''Number of cracks using averaging
+    '''
+    def _get_number_of_cracks_avg(self):
+        return np.sum(self.crack_filter_avg)
 
     crack_spacing_avg = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
     @cached_property
@@ -534,6 +563,11 @@ class AramisCDT(HasTraits):
     @cached_property
     def _get_crack_arr(self):
         return self.d_ux_arr[np.where(self.crack_filter)]
+
+    crack_avg_arr = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
+    @cached_property
+    def _get_crack_avg_arr(self):
+        return self.d_ux_arr_avg[np.where(self.crack_filter_avg)]
 
     crack_field_arr = Property(Array, depends_on='aramis_info.data_dir, +params_changed')
     @cached_property
@@ -550,6 +584,47 @@ class AramisCDT(HasTraits):
     '''
 
     ad_channels_lst = List
+    '''List of tuples (undeformed, deformed) obtained from AD channels in 
+    aramis file header
+    '''
+
+    number_of_missing_facets_t = List
+    '''Number of missing facets in time (missing, unidentified, not satisfying
+    conditions, destroyed by crack)
+    '''
+
+    control_strain_t = Array()
+    '''Control strain measured in the middle of y
+    '''
+
+    init_step_avg_lst = List()
+    '''List of steps (list length is equal to number of cracks at the 
+    w_detect_step) when the crack initiate
+    '''
+    init_step_lst = List()
+
+    crack_width_avg_t = Array
+    crack_stress_t = Array
+
+    force_t_mult_coef = Float(100.0)
+    '''Multiplication coefficient to obtain force_t from AD channel value
+    '''
+
+    # TODO: load from exp db
+    force_t_to_stress = Float(100.0 * 20.0)
+
+    force_t = Property(Array)
+    '''Force evaluated from AD channel value (default channel for force_t is 0)
+    '''
+    def _get_force_t(self):
+        return (self.ad_channels_arr[:, 0, 2] -
+                self.ad_channels_arr[:, 0, 1]) * self.force_t_mult_coef
+
+    stress_t = Property(Array)
+    '''Concrete stress
+    '''
+    def _get_stress_t(self):
+        return self.force_t * 1000 / self.force_t_to_stress
 
     ad_channels_arr = Property(Array)
     def _get_ad_channels_arr(self):
@@ -561,7 +636,7 @@ class AramisCDT(HasTraits):
 
     def __number_of_cracks_t(self):
         self.number_of_cracks_t = np.append(self.number_of_cracks_t,
-                                           np.sum(self.crack_filter_avg))
+                                           self.number_of_cracks_avg)
 
     def __decompile_ad_channels(self):
         fname = '%s%d.txt' % (self.aramis_info.basename, self.evaluated_step)
@@ -578,12 +653,150 @@ class AramisCDT(HasTraits):
     '''Run analysis of all steps in time
     '''
     def _run_t_fired(self):
+        self.number_of_cracks_t = np.array([])
+        self.ad_channels_lst = []
+        self.number_of_missing_facets_t = []
+        self.control_strain_t = np.array([])
+        print self.ux_arr.shape
         for step, step_file in zip(self.aramis_info.step_list, self.aramis_info.file_list):
             self.evaluated_step = step
             if self.ad_channels_read:
                 self.__decompile_ad_channels()
             if self.number_of_cracks_t_analyse:
                 self.__number_of_cracks_t()
+            self.control_strain_t = np.append(self.control_strain_t,
+                                            (self.ux_arr[20, -10] - self.ux_arr[20, 10]) /
+                                            (self.x_arr[20, -10] - self.x_arr[20, 10]))
+            self.number_of_missing_facets_t.append(np.sum(np.isnan(self.data_array).astype(int)))
+        print 'control_strain_t', self.control_strain_t
+#         import matplotlib.pyplot as plt
+#         plt.figure()
+#         plt.plot(self.control_strain_t)
+#         plt.show()
+
+
+    w_detect_mask_avg = Property(Array, depends_on='w_detect_step')
+    '''Mask of cracks identified in w_detect_step and used for backward 
+    identification of the crack initialization.
+    '''
+    @cached_property
+    def _get_w_detect_mask_avg(self):
+        self.evaluated_step = self.step_list[self.w_detect_step]
+        return self.crack_filter_avg
+
+    w_detect_mask = Property(Array, depends_on='w_detect_step')
+    '''Mask of cracks identified in w_detect_step and used for backward 
+    identification of the crack initialization.
+    '''
+    @cached_property
+    def _get_w_detect_mask(self):
+        self.evaluated_step = self.step_list[self.w_detect_step]
+        return self.crack_filter
+
+    run_back = Button('Run back in time')
+    '''Run analysis of all steps in time
+    '''
+    def _run_back_fired(self):
+        # todo: better
+        x = self.x_arr[20, :]
+        print x
+        # import matplotlib.pyplot as plt
+        # plt.rc('font', size=25)
+        step = self.step_list[self.w_detect_step]
+        crack_step_avg = np.zeros_like(self.w_detect_mask_avg, dtype=int)
+        crack_step_avg[self.w_detect_mask_avg] = step
+        crack_step = np.zeros_like(self.w_detect_mask, dtype=int)
+        crack_step[self.w_detect_mask] = step
+        step -= 1
+        self.crack_width_avg_t = np.zeros((np.sum(self.w_detect_mask_avg),
+                                       len(self.aramis_info.step_list)))
+
+        self.crack_width_t = np.zeros((np.sum(self.w_detect_mask),
+                                       len(self.aramis_info.step_list)))
+        print 'fasdf', self.crack_width_t.shape
+        self.crack_stress_t = np.zeros((np.sum(self.w_detect_mask_avg),
+                                       len(self.aramis_info.step_list)))
+        # plt.figure()
+        while step:
+            self.evaluated_step = step
+            mask = self.crack_filter_avg * self.w_detect_mask_avg
+            crack_step_avg[mask] = step
+            mask = self.crack_filter * self.w_detect_mask
+            crack_step[mask] = step
+            # if number of cracks = 0 break
+            # plt.plot(x, self.d_ux_arr_avg, color='grey')
+#             if step == 178:
+# #                 print '[asdfasfas[', self.d_ux_arr_avg
+#                 plt.plot(x, self.d_ux_arr_avg, 'k-', linewidth=3, zorder=10000)
+            # if np.any(mask) == False:
+                # print mask
+            #    break
+            if step == 0:
+                break
+            self.crack_width_avg_t[:, step] = self.d_ux_arr_avg[crack_step_avg > 0]
+            self.crack_width_t[:, step] = self.d_ux_arr[crack_step > 0]
+            # y = self.ad_channels_arr[:, :, 2] - self.ad_channels_arr[:, :, 1]
+            # self.crack_stress_t[:, step] = y[:, 0][step] * 1e5 / (140 * 60)
+            step -= 1
+        # y_max_lim = plt.gca().get_ylim()[-1]
+#         plt.vlines(x[:-1], [0], self.w_detect_mask_avg * y_max_lim,
+#                    color='magenta', linewidth=1, zorder=10)
+#         plt.xlim(0, 527)
+#         plt.ylim(0, 0.25)
+#         print 'initializationinit_steps time/step of the crack' , crack_step_avg[crack_step_avg > 0]
+#
+        init_steps_avg = crack_step_avg[crack_step_avg > 0]
+        init_steps = crack_step[crack_step > 0]
+#         # crack width
+#         plt.figure()
+#         # plt.title('crack width vs. control strain')
+#         plt.plot(self.control_strain_t * 100, self.crack_width_avg_t.T)
+#         plt.xlim(0, 0.71)
+#         plt.ylim(0, 0.25)
+#         plt.xlabel('control strain [%]')
+#         plt.ylabel('crack width [mm]')
+#         plt.tight_layout()
+#         print self.crack_filter_avg
+#         print self.crack_filter_avg.max()
+#
+#         for i, s in enumerate(init_steps_avg):
+#             self.crack_width_avg_t[i, :s] = 0
+#         plt.figure()
+#         # plt.title('crack width vs. control strain')
+#         plt.plot(self.control_strain_t * 100, self.crack_width_avg_t.T)
+#         plt.xlim(0, 0.71)
+#         plt.ylim(0, 0.25)
+#         plt.xlabel('control strain [%]')
+#         plt.ylabel('crack width [mm]')
+#         plt.tight_layout()
+#         print self.crack_filter_avg
+#         print self.crack_filter_avg.max()
+#
+#         plt.figure()
+#         plt.title('nominal stress vs crack width')
+#         plt.plot(self.crack_width_avg_t.T, self.crack_stress_t.T)
+#
+#         #
+        self.init_step_avg_lst = init_steps_avg.tolist()
+        self.init_step_lst = init_steps.tolist()
+#         init_steps_avg.sort()
+#         x = np.hstack((0, np.repeat(init_steps_avg, 2), self.aramis_info.last_step))
+#         y = np.repeat(np.arange(init_steps_avg.size + 1), 2)
+#         print x, y
+#
+#         plt.figure()
+#         plt.title('number of cracks vs. step')
+#         plt.plot(x, y)
+#
+#         plt.figure()
+#         x = np.hstack((0, np.repeat(init_steps_avg, 2), self.aramis_info.last_step))
+#         x = self.control_strain_t[x]
+#         y = np.repeat(np.arange(init_steps_avg.size + 1), 2)
+#         plt.title('number of cracks vs. control strain')
+#         plt.plot(x, y)
+
+        # plt.show()
+
 
     view = View(
                 HGroup(
@@ -594,17 +807,23 @@ class AramisCDT(HasTraits):
                              springy=True),
                        ),
                 Item('data_array_init_shape', label='data shape', style='readonly'),
-                Item('n_px_facette_size_x'),
-                Item('n_px_facette_distance_x'),
-                Item('n_px_facette_size_y'),
-                Item('n_px_facette_distance_y'),
+                Item('n_px_facet_size_x'),
+                Item('n_px_facet_distance_x'),
+                Item('n_px_facet_size_y'),
+                Item('n_px_facet_distance_y'),
                 Item('w_detect_step'),
                 Item('integ_radius'),
+                Item('d_ux_threshold'),
+                Item('dd_ux_threshold'),
+                Item('ddd_ux_threshold'),
+                Item('dd_ux_avg_threshold'),
+                Item('ddd_ux_avg_threshold'),
                 'transform_data',
                 Group(
                       'number_of_cracks_t_analyse',
                       'ad_channels_read',
                       UItem('run_t'),
+                      UItem('run_back'),
                       ),
                 )
 
@@ -615,13 +834,21 @@ if __name__ == '__main__':
     elif platform.system() == 'Windows':
         aramis_dir = os.path.join(r'E:\_linux_data\aachen\ARAMIS_data_IMB\01_ARAMIS_Daten')
     data_dir = os.path.join(aramis_dir, 'Probe-1-Ausschnitt-Xf15a1-Yf5a4')
-    AI = AramisInfo(data_dir='/media/data/_linux_data/aachen/IMB_orig/Probe-2/Probe-2')
+    data_dir = '/media/data/_linux_data/aachen/IMB_orig/Probe-2/Probe-2'
+    # data_dir = '/media/data/_linux_data/aachen/IMB_orig/Probe-1-final/Probe-1/'
+    AI = AramisInfo(data_dir=data_dir)
     ACT = AramisCDT(aramis_info=AI,
-                   evaluated_step=400)
+                   evaluated_step=400,
+                   integ_radius=1,
+                   transform_data=True,
+                   w_detect_step=472)  # 443
 
     print ACT.data_array
-    ACT.transform_data = True
-    print ACT.data_array
+
+    print ACT.crack_filter_avg
+
+    ACT._run_t_fired()
+    ACT._run_back_fired()
 
 #     view = View(
 #                 UItem('aramis_info.data_dir@'),
