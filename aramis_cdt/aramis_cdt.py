cf3266f915b5f125dae6b88d6cd800a006ca933e
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2015-04-30 10:22:54 +0200

    Code formatting

--- aramis_cdt/aramis_cdt.py
+++ aramis_cdt/aramis_cdt.py
@@ -306,8 +306,8 @@ class AramisCDT(HasTraits):
                 break
             print step
             self.aramis_data.current_step = step
-            d_ux_avg_mask = (self.aramis_data.d_ux_avg[
-                             1:] + self.aramis_data.d_ux_avg[:-1]) * 0.5 > self.d_ux_avg_threshold
+            d_ux_avg_mask = (self.aramis_data.d_ux_avg[1:] +
+                             self.aramis_data.d_ux_avg[:-1]) * 0.5 > self.d_ux_avg_threshold
             mask = d_ux_avg_mask * m
             crack_step_avg[mask] = step
 
