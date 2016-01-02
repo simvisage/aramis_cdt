7c70eab20eaff54f2b05a1a3d577e5df81febb35
ale-x <ascholzen@imb.rwth-aachen.de>
2016-01-02 16:30:52 +0100

    add subplot of 'delta_ux_arr' for verification

--- aramis_cdt/aramis_view2d.py
+++ aramis_cdt/aramis_view2d.py
@@ -73,12 +73,12 @@ class AramisPlot2D(HasTraits):
         aramis_cdt = self.aramis_cdt
         fig = self.figure
         fig.clf()
-        ax = fig.add_subplot(2, 2, 1)
 
+        ax = fig.add_subplot(2, 2, 1)
         ax.plot(aramis_data.i_cut.T, aramis_data.d_ux.T, color='black')
+        ax.plot(aramis_data.i_cut[0, :], aramis_data.d_ux_avg, color='red', linewidth=2)
         # for x in aramis_data.d_ux:
         #    ax.plot(aramis_data.i_cut.T, np.convolve(x, np.ones(10) / 10., 'same'), color='green', linewidth=0.3)
-        ax.plot(aramis_data.i_cut[0, :], aramis_data.d_ux_avg, color='red', linewidth=2)
         y_max_lim = ax.get_ylim()[-1]
         ax.vlines(aramis_data.i_cut[0, :-1], [0], aramis_cdt.crack_filter_avg * y_max_lim,
                  color='magenta', linewidth=1, zorder=10)
@@ -102,8 +102,17 @@ class AramisPlot2D(HasTraits):
         ax3.set_title('dd_ux, ddd_ux')
 
         ax = fig.add_subplot(2, 2, 4)
-        from aramis_data import get_d
-        ir = aramis_data.integ_radius
+        ax.plot(aramis_data.i_cut.T, aramis_data.delta_ux_arr.T, color='black')
+        ax.plot(aramis_data.i_cut[0, :], aramis_data.delta_ux_arr_avg, color='red', linewidth=2)
+        y_max_lim = ax.get_ylim()[-1]
+        ax.vlines(aramis_data.i_cut[0, :-1], [0], aramis_cdt.crack_filter_avg * y_max_lim,
+                 color='magenta', linewidth=1, zorder=10)
+        ax.set_title('delta_ux')
+
+
+#         ax = fig.add_subplot(2, 2, 4)
+#         from aramis_data import get_d
+#         ir = aramis_data.integ_radius
 #         ax.plot(aramis_data.x_arr_0.T[ir:-ir, :],
 #                 (get_d(aramis_data.x_arr_0 + aramis_data.ux_arr, ir).T - get_d(aramis_data.x_arr_0, ir).T)[ir:-ir, :], color='black')
 #        ax.plot(aramis_data.x_arr_0.T[ir:-ir, :],
